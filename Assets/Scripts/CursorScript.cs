dd07941d5cb1b17cf46e130a95ca29f9cc2c39af
Sicho84 <sacha.galgon@gmail.com>
2017-10-10 19:11:15 +0200

    Trampoline
    Added a Trampoline including assets and made some changes to the testlevel Level0

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Set Cursor to be visible
+        Cursor.visible = true;
+    }
+}
---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour {
+
+    public float trampolineForce = 1000f;
+    private GameObject player;
+    private Rigidbody2D playerrigi;
+    public AudioClip bounceSound; //sound the trampoline makes
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            player = collision.gameObject;
+            playerrigi = player.GetComponent<Rigidbody2D>();
+            Vector3 velocity;
+            velocity = playerrigi.velocity;
+            
+            velocity = new Vector3(velocity.x, 0, velocity.z);
+            playerrigi.velocity = velocity;
+            playerrigi.AddForce(Vector3.up * trampolineForce);
+            AudioSource.PlayClipAtPoint(bounceSound, transform.position);
+
+
+        }
+    }
+
+}
